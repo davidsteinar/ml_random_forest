b56bf1f55e25aa83498e1c4cd9ccd93ecfc20e6a
Hesam <hesamp@kth.se>
2017-01-07 13:07:13 +0100

    changes to random feature to make it work with gini split

--- randomFeature.py
+++ randomFeature.py
@@ -15,27 +15,29 @@ def randomIndices(df, predict, m):
     done = False
     features = []
     used = np.zeros(df.shape[1])
+    used[predict] = 1
 
     # pick random indices, check that they are unique
     # and not the predicted variable
     while(not done):
         feature_indx = np.random.randint(low=0, high=df.shape[1])
-        if(not used[feature_indx] and feature_indx != predict):
+        if(not used[feature_indx]):
             used[feature_indx] = 1
             features.append(feature_indx)
         if(len(features) == m):
             done = True
+    features.append(predict)
     return np.asarray(features)
 
 
 def randomFeature(df, predict, m):
     # pick features at random
     indx = randomIndices(df, predict, m)
-    return df.iloc[:, indx]
+    return [df.iloc[:, indx], indx]
 
 
 def testRandomFeature():
     df = pd.read_csv('iris.csv')
     bag = bagging(df, 5)
-    print (bag[0][0])
+    print (df)
     print(randomFeature(bag[0][0], 4, 2))
