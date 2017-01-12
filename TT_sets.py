d4f49f5b087e81f4c57fba0e3a2b5b28d4d2a16d
Hesam <hesamp@kth.se>
2017-01-12 12:20:13 +0100

    cosmetics

--- TT_sets.py
+++ TT_sets.py
@@ -1,47 +1,12 @@
-
-# coding: utf-8
-
-# In[171]:
-
-# Pantelis
+# Author Pantelis
 
 import pandas as pd
 
-def TT_sets(df, p):
-
-    train_set = df.sample(frac = p)
-    test_set = df.drop(train_set.index[:])  
+'''
+Will split dataset df into training set with fraction
+q and train set with fraction 1-q.
+'''
+def TT_sets(df, q):
+    df_train = df.sample(frac = q)
+    df_test = df.drop(train_set.index[:])
     return train_set, test_set
-
-
-# In[ ]:
-
-
-
-
-# In[172]:
-
-len(df)
-
-
-# In[173]:
-
-df = pd.read_csv('biopsy.csv')
-p = 0.8
-x,y = sets(df, p)
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
